bfa44f47b3703f4268585e61be9835eb1727264d
caseyjfer <caseyjfer@gmail.com>
2019-10-29 14:04:22 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { SkillListComponent } from './skill-list/skill-list.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+      { path: '', component: SkillListComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    SkillListComponent
   ],
   bootstrap: [ AppComponent ]
 })
