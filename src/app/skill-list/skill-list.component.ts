9e3774058668e7a2da19491f095966618dbde9e5
caseyjfer <caseyjfer@gmail.com>
2019-10-29 13:53:49 -0400

    skills

---
+++ src/app/skill-list/skill-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { skills } from '../skills';
+
+@Component({
+  selector: 'app-skill-list',
+  templateUrl: './skill-list.component.html',
+  styleUrls: ['./skill-list.component.css']
+})
+export class SkillListComponent {
+  skills = skills;
+
+  share() {
+    window.alert('The skill has been shared!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/skills.ts
@@ -0,0 +1,49 @@
+export const skills = [
+  {
+    name: 'Cleaning',
+    description: 'Picking up messes and generally maintaining cleanliness in life'
+  },
+  {
+    name: 'Cooking',
+    description: 'Planning, shopping and preparing meals'
+  },
+  {
+    name: 'Communication',
+    description: 'Communicating effectively and maturely'
+  },
+  {
+    name: 'Landscaping',
+    description: 'Maintaining the outdoor natural areas of the home'
+  },
+  {
+    name: 'Handicraft',
+    description: 'Maintaining household equipment'
+  },
+  {
+    name: 'Hygiene',
+    description: 'Maintaining personal cleanliness and tidiness'
+  },
+  {
+    name: 'Work',
+    description: 'Holding down a job and being effective at it'
+  },
+  {
+    name: 'Budgeting',
+    description: 'Give every dollar a name'
+  },
+  {
+    name: 'Mental Health',
+    description: 'Keep your sanity'
+  },
+  {
+    name: 'Physique',
+    description: 'Build and maintain your temple'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
